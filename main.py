90b83635a8e49c44896d1745472cb715241b11e5
Muhammad Rokonuzzaman <rokonmagura@gmail.com>
2025-01-28 06:29:30 +0600

    Update main.py

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ API_KEY_5 = os.getenv('YOUTUBE_API_5')
 API_KEY_6 = os.getenv('YOUTUBE_API_6')
 API_KEY_7 = os.getenv('YOUTUBE_API_7')
 API_KEY_8 = os.getenv('YOUTUBE_API_8')
+print(API_KEY_1)
 
         
 def get_user_agent():
