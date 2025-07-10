9689a1f4f9a99a682f99ff440691ad490cca7b55
Muhammad Rokonuzzaman <rokonmagura@gmail.com>
2025-07-10 19:16:15 +0600

    Improve overall code

--- main.py
+++ main.py
@@ -10,9 +10,6 @@ now = datetime.datetime.now()
 # Define the path to the cookies.txt file
 cookies_file_path = 'cookies.txt'
 
-# No stream URL found message
-no_stream = 'https://raw.githubusercontent.com/time2shine/IPTV/refs/heads/master/no_stream.mp4'
-
 # Check if the file exists
 if not os.path.exists(cookies_file_path):
     raise FileNotFoundError(f"The file {cookies_file_path} does not exist")
@@ -35,7 +32,7 @@ def get_stream_url(url):
     """Retrieve YouTube stream URL using yt-dlp with bot mitigation."""
     ydl_opts = {
         'format': 'best',
-        'cookiefile': 'cookies.txt',
+        'cookiefile': cookies_file_path,
         'force_ipv4': True,
         'retries': 10,
         'fragment_retries': 10,
@@ -84,30 +81,11 @@ def save_m3u_file(output_data, filename="YT_playlist.m3u"):
             file.write(data + "\n")
     print(f"M3U file saved as {filename}")
 
-def get_channel_info(youtube, channel_id):
-    "Fetch the channel information such as logo."
-    try:
-        request = youtube.channels().list(
-            part="snippet",
-            id=channel_id
-        )
-        response = request.execute()
-        if 'items' in response and response['items']:
-            snippet = response['items'][0]['snippet']
-            thumbnails = snippet.get('thumbnails', {})
-            channel_logo = thumbnails.get('default', {}).get('url', '')
-            return channel_logo
-        else:
-            return None
-    except Exception as e:
-        print(f"Error fetching channel info for {channel_id}: {e}")
-        return None
-
 def get_latest_live_link(youtube, channel_id):
     """Fetch the latest live video link from the specified YouTube channel."""
     try:
         request = youtube.search().list(
-            part="snippet",
+            part="id",  # only id for minimal quota usage
             channelId=channel_id,
             eventType="live",
             type="video",
@@ -117,81 +95,93 @@ def get_latest_live_link(youtube, channel_id):
         response = request.execute()
         if 'items' in response and response['items']:
             video_id = response['items'][0]['id']['videoId']
-            live_link = f"https://www.youtube.com/watch?v={video_id}"
-            return live_link
+            return f"https://www.youtube.com/watch?v={video_id}"
         else:
-            return None, None  # No live video currently available
+            return None  # No live video currently available
     except Exception as e:
         print(f"Error fetching latest live link for {channel_id}: {e}")
-        return None, None
+        return None
 
-# Combined mapping of channel IDs to their metadata
+# Your channel metadata dictionary
 channel_metadata = {
     # --- News Channels (Bangladesh) ---
     'UCxHoBXkY88Tb8z1Ssj6CWsQ': {  # Somoy News
         'channel_number': 101,
         'group_title': 'News',
         'channel_name': 'Somoy News',
+        'channel_logo': 'https://yt3.googleusercontent.com/7F-3_9yjPiMJzBAuD7niglcJmFyXCrFGSEugcEroFrIkxudmhiZ9i-Q_pW4Zrn2IiCLN5dQX8A=s160-c-k-c0x00ffffff-no-rj',
     },
     'UCN6sm8iHiPd0cnoUardDAnw': {  # Jamuna TV
         'channel_number': 102,
         'group_title': 'News',
         'channel_name': 'Jamuna TV',
+        'channel_logo': 'https://yt3.ggpht.com/54prTx28YpPxSpk_PfJGuOfQgcZbNdvbfk0adGePrAvINO4Mo9_bw3j-J4seXn6hNGuMr1ck=s176-c-k-c0x00ffffff-no-rj-mo',
     },
     'UCWVqdPTigfQ-cSNwG7O9MeA': {  # EKHON TV
         'channel_number': 103,
         'group_title': 'News',
         'channel_name': 'EKHON TV',
+        'channel_logo': 'https://yt3.googleusercontent.com/66cO7vPXs2Xssf6fq2cn90oDsJ3OFMThb57qfRkRMjaSqg3ouTG6m9WQKZFg6GmUS5G8wkPu7ik=s72-c-k-c0x00ffffff-no-rj',
     },
     'UCHLqIOMPk20w-6cFgkA90jw': {  # Channel 24
         'channel_number': 104,
         'group_title': 'News',
         'channel_name': 'Channel 24',
+        'channel_logo': 'https://yt3.googleusercontent.com/8Q8MCd6ypr2Hzbp60VE_stJPl063kQYfeTxdIQkAXRfhdzxByLl0sJYHsk43uTM4W_cOzwcbPQ=s160-c-k-c0x00ffffff-no-rj',
     },
     'UCtqvtAVmad5zywaziN6CbfA': {  # Ekattor TV
         'channel_number': 105,
         'group_title': 'News',
         'channel_name': 'Ekattor TV',
+        'channel_logo': 'https://yt3.googleusercontent.com/M8Rqad6_uN86mMSvd9KGkE5G2mrVAgvfTV-VCsQb6jhfF5hEbcQCEJiInih4wb2fMQ_RG7Ku=s160-c-k-c0x00ffffff-no-rj',
     },
     'UCATUkaOHwO9EP_W87zCiPbA': {  # Independent Television
         'channel_number': 106,
         'group_title': 'News',
         'channel_name': 'Independent Television',
+        'channel_logo': 'https://yt3.googleusercontent.com/JO8MicN497ze8WVXcu-wmA2WAMqhO8UIQhslV3VhiRu1kaQU3r9nOB4IVkmUt0ALC23DVSSp=s160-c-k-c0x00ffffff-no-rj',
     },
     'UCUvXoiDEKI8VZJrr58g4VAw': {  # DBC NEWS
         'channel_number': 107,
         'group_title': 'News',
         'channel_name': 'DBC News',
+        'channel_logo': 'https://yt3.googleusercontent.com/ytc/AIdro_mcjICfjqYwOn4eljNmtsZLBpOmW0t0JlzJUhXSslAkBVo=s160-c-k-c0x00ffffff-no-rj',
     },
     'UC2P5Fd5g41Gtdqf0Uzh8Qaw': {  # Rtv News
         'channel_number': 108,
         'group_title': 'News',
         'channel_name': 'Rtv News',
+        'channel_logo': 'https://yt3.googleusercontent.com/Jw5N_GsIqsDis5cvYkAlJZFU2Z3m_6q6GaTdmUK0hJ4bsadl4He51sFu1LoCGKmf3QiTG-9P5G4=s160-c-k-c0x00ffffff-no-rj',
     },
     'UC0V3IJCnr6ZNjB9t_GLhFFA': {  # NTV Live
         'channel_number': 109,
         'group_title': 'News',
         'channel_name': 'NTV Live',
+        'channel_logo': 'https://yt3.googleusercontent.com/NgVaE3RsOhC9cIV_6kTp0h2ikrHIFG8QPJF5IrRg_nPiQbQeG-dzK5SHLmi_MDEyVNj73aeSHg=s160-c-k-c0x00ffffff-no-rj',
     },
     'UC8NcXMG3A3f2aFQyGTpSNww': {  # Channel i News
         'channel_number': 110,
         'group_title': 'News',
         'channel_name': 'Channel i News',
+        'channel_logo': 'https://yt3.googleusercontent.com/Yv5b1h00Dj_JkJsCD8IEuodFSRq3hFiFi0AQc-W5JrJkrAyX98lxWGUUhjqZzOA-NU5LwUzU=s160-c-k-c0x00ffffff-no-rj',
     },
     'UCb2O5Uo4a26CdTE7_2QA-jA': {  # NEWS24 Television
         'channel_number': 111,
         'group_title': 'News',
         'channel_name': 'NEWS24 Television',
-    },
-    'UCmCCTsDl-eCKw91shC7ZmMw': {  # Desh TV News
-        'channel_number': 112,
-        'group_title': 'News',
-        'channel_name': 'Desh TV News',
+        'channel_logo': 'https://yt3.googleusercontent.com/bn7BHmnbqkIJMoQWzUk3K5Wzzt_mVgVdjQ5XV8PWdQS18_w4ZVZOSFwe_ZIKaO3KitQPVuQczA=s160-c-k-c0x00ffffff-no-rj',
     },
     'UCRt2klyaxgx89vPF8cFMfnQ': {  # Kalbela News
         'channel_number': 113,
         'group_title': 'News',
         'channel_name': 'Kalbela News',
+        'channel_logo': 'https://yt3.googleusercontent.com/DdtUl06VkqvZvr9aDFP6iX-qxWxV5Aqlk1d4mTUdD1E34wwX333DKo56iJiSJ3hojEeeW_kVzEc=s160-c-k-c0x00ffffff-no-rj',
+    },
+    'UC9Rgo0CrNyd7OWliLekqqGA': {  # ATN News
+        'channel_number': 114,
+        'group_title': 'News',
+        'channel_name': 'ATN News',
+        'channel_logo': 'https://yt3.googleusercontent.com/ceDID2koAfvhvykNroiBjW4SkTrkGVrjFk1EkWRz8SZdhV_dFsCtuJ3w9l0D-y06VgHjvB-WqA=s160-c-k-c0x00ffffff-no-rj',
     },
 
     # --- News Channels (India) ---
@@ -199,26 +189,31 @@ channel_metadata = {
         'channel_number': 121,
         'group_title': 'News',
         'channel_name': 'News18 Bangla',
+        'channel_logo': 'https://yt3.googleusercontent.com/0pVAsTdgTX-iREI9xZUMsYbqjpclujOiC7mocZIvLlWYVmhKRP131kzwVzM-i63lQz2YMMXo=s160-c-k-c0x00ffffff-no-rj',
     },
     'UCdF5Q5QVbYstYrTfpgUl0ZA': {  # Zee 24 Ghanta
         'channel_number': 122,
         'group_title': 'News',
         'channel_name': 'Zee 24 Ghanta',
+        'channel_logo': 'https://yt3.googleusercontent.com/ou3JhDnH8ChdzRKooH5hGjTRGKpr9dGhi7lv7QW2zgmrnme0HbPKc8qI3yu8ZdI6NZna-CdJFw=s160-c-k-c0x00ffffff-no-rj',
     },
     'UCHCR4UFsGwd_VcDa0-a4haw': {  # TV9 Bangla
         'channel_number': 123,
         'group_title': 'News',
         'channel_name': 'TV9 Bangla',
+        'channel_logo': 'https://yt3.googleusercontent.com/d8QNkJ7Jby9hVSTm67-E4nfbI-7CTgP262NPVGfYpoTZaxLw7uAOPxs5dJtARjEFQijsRsuiFQ=s160-c-k-c0x00ffffff-no-rj',
     },
     'UCajVjEHDoVn_AHsunUZz_EQ': {  # Republic Bangla
         'channel_number': 124,
         'group_title': 'News',
         'channel_name': 'Republic Bangla',
+        'channel_logo': 'https://yt3.googleusercontent.com/ytc/AIdro_nvgVv3ZKeS6nLI_ZGZw-pZpt88bQpFPZNbUOADI3Bvz9k=s160-c-k-c0x00ffffff-no-rj',
     },
     'UCv3rFzn-GHGtqzXiaq3sWNg': {  # ABP ANANDA
         'channel_number': 125,
         'group_title': 'News',
         'channel_name': 'ABP Ananda',
+        'channel_logo': 'https://yt3.googleusercontent.com/ytc/AIdro_kLTXHZwzmSJJh3W6bm_134dfLEh_vjEpjL8QE8Yn4l6cs=s160-c-k-c0x00ffffff-no-rj',
     },
 
     # --- International News Channels ---
@@ -226,21 +221,25 @@ channel_metadata = {
         'channel_number': 150,
         'group_title': 'International News',
         'channel_name': 'Al Jazeera English',
+        'channel_logo': '',
     },
     'UC7fWeaHhqgM4Ry-RMpM2YYw': {  # TRT World
         'channel_number': 151,
         'group_title': 'International News',
         'channel_name': 'TRT World',
+        'channel_logo': '',
     },
     'UCknLrEdhRCp1aegoMqRaCZg': {  # DW News
         'channel_number': 152,
         'group_title': 'International News',
         'channel_name': 'DW News',
+        'channel_logo': '',
     },
     'UCUMZ7gohGI9HcU9VNsr2FJQ': {  # Bloomberg Originals
         'channel_number': 153,
         'group_title': 'International News',
         'channel_name': 'Bloomberg Originals',
+        'channel_logo': '',
     },
 
     # --- Entertainment Channels ---
@@ -248,41 +247,49 @@ channel_metadata = {
         'channel_number': 201,
         'group_title': 'Entertainment',
         'channel_name': 'Bongo',
+        'channel_logo': '',
     },
     'UCvoC1eVphUAe7a0m-uuoPbg': {  # Bongo Movies
         'channel_number': 202,
         'group_title': 'Entertainment',
         'channel_name': 'Bongo Movies',
+        'channel_logo': '',
     },
     'UCsr6QVeLlkitleHoS0T4IxQ': {  # Banglavision DRAMA
         'channel_number': 203,
         'group_title': 'Entertainment',
         'channel_name': 'Banglavision DRAMA',
+        'channel_logo': '',
     },
     'UCEwIUtFBhaI2L2PuKv0KL2g': {  # Classic Mr Bean
         'channel_number': 204,
         'group_title': 'Entertainment',
         'channel_name': 'Classic Mr Bean',
+        'channel_logo': '',
     },
     'UCkAGrHCLFmlK3H2kd6isipg': {  # Mr Bean
         'channel_number': 205,
         'group_title': 'Entertainment',
         'channel_name': 'Mr Bean',
+        'channel_logo': '',
     },
     'UC5V8MdQsT_gLlw8rTyf7jVQ': {  # ClipZone: Comedy Callbacks
         'channel_number': 206,
         'group_title': 'Entertainment',
         'channel_name': 'ClipZone: Comedy Callbacks',
+        'channel_logo': '',
     },
     'UCQt7Z-GE0wF8AzFNvQEqjvg': {  # ClipZone: Heroes & Villains
         'channel_number': 207,
         'group_title': 'Entertainment',
         'channel_name': 'ClipZone: Heroes & Villains',
+        'channel_logo': '',
     },
     'UCw7SNYrYei7F5ttQO3o-rpA': {  # Disney Channel
         'channel_number': 208,
         'group_title': 'Entertainment',
         'channel_name': 'Disney Channel',
+        'channel_logo': '',
     },
 
     # --- Religious Channels ---
@@ -290,6 +297,7 @@ channel_metadata = {
         'channel_number': 251,
         'group_title': 'Religious',
         'channel_name': 'Madani Channel Bangla Live',
+        'channel_logo': '',
     },
 
     # --- Wildlife and Educational Channels ---
@@ -297,11 +305,13 @@ channel_metadata = {
         'channel_number': 301,
         'group_title': 'Educational',
         'channel_name': 'Nat Geo Animals',
+        'channel_logo': '',
     },
     'UCpVm7bg6pXKo1Pr6k5kxG9A': {  # National Geographic
         'channel_number': 302,
         'group_title': 'Educational',
         'channel_name': 'National Geographic',
+        'channel_logo': '',
     },
 
     # --- Kids Channels ---
@@ -309,106 +319,106 @@ channel_metadata = {
         'channel_number': 401,
         'group_title': 'Kids',
         'channel_name': 'Boomerang UK',
+        'channel_logo': '',
     },
     'UCiBigY9XM-HaOxUc269ympg': {  # Green Gold TV - Official Channel
         'channel_number': 402,
         'group_title': 'Kids',
         'channel_name': 'Green Gold TV',
+        'channel_logo': '',
     },
     'UCu7IDy0y-ZA0qaG51wrQY6w': {  # Curious George Official
         'channel_number': 403,
         'group_title': 'Kids',
         'channel_name': 'Curious George Official',
+        'channel_logo': '',
     },
     'UCVzLLZkDuFGAE2BGdBuBNBg': {  # Bluey - Official Channel
         'channel_number': 404,
         'group_title': 'Kids',
         'channel_name': 'Bluey - Official Channel',
+        'channel_logo': '',
     },
     'UCoBpC9J2EcbAMprw7YjC93A': {  # The Amazing World of Gumball
         'channel_number': 405,
         'group_title': 'Kids',
         'channel_name': 'The Amazing World of Gumball',
+        'channel_logo': '',
     },
     'UCktaw9L-f65LzUUdjmCFkbQ': {  # Disney XD
         'channel_number': 406,
         'group_title': 'Kids',
         'channel_name': 'Disney XD',
+        'channel_logo': '',
     },
     'UCNcdbMyA59zE-Vk668bKWOg': {  # Disney Jr.
         'channel_number': 407,
         'group_title': 'Kids',
         'channel_name': 'Disney Jr.',
+        'channel_logo': '',
     },
     'UCWE_ywN-0aeFdGVpLQ6mIwg': {  # WildBrain Bananas
         'channel_number': 408,
         'group_title': 'Kids',
         'channel_name': 'WildBrain Bananas',
+        'channel_logo': '',
     },
     'UCu59yAFE8fM0sVNTipR4edw': {  # Masha and The Bear
         'channel_number': 409,
         'group_title': 'Kids',
         'channel_name': 'Masha and The Bear',
+        'channel_logo': '',
     },
 }
 
+
 def main(api_key):
-    """Main function to fetch and format live video links for multiple channels."""
     youtube = get_youtube_service(api_key)
     output_data = []
 
     for channel_id, metadata in channel_metadata.items():
-        # Retrieve channel metadata
         channel_number = metadata.get('channel_number', '0')
         group_title = metadata.get('group_title', 'Others')
         channel_name = metadata.get('channel_name', 'Unknown')
+        channel_logo = ''  # Skip logo fetching
 
-        # Fetch channel info (logo)
-        channel_logo = get_channel_info(youtube, channel_id)
-
-        # Fetch the latest live link
         live_link = get_latest_live_link(youtube, channel_id)
-        if live_link:
-            m3u8_link = get_stream_url(live_link)
-            if m3u8_link:
-                formatted_info = format_live_link(
-                    channel_name, channel_logo, m3u8_link, channel_number, group_title
-                )
-            else:
-                # Include the channel with an empty URL if m3u8 link isn't found
-                formatted_info = format_live_link(
-                    channel_name, channel_logo, '', channel_number, group_title
-                )
-        else:
-            # Include the channel with an empty URL if no live video is available
-            formatted_info = format_live_link(
-                channel_name, channel_logo, '', channel_number, group_title
-            )
+        if not live_link:
+            print(f"Skipping {channel_name}: no live video found")
+            continue
 
+        m3u8_link = get_stream_url(live_link)
+        if not m3u8_link:
+            print(f"Skipping {channel_name}: no stream link found")
+            continue
+
+        formatted_info = format_live_link(
+            channel_name, channel_logo, m3u8_link, channel_number, group_title
+        )
         output_data.append(formatted_info)
 
     if output_data:
         save_m3u_file(output_data)
     else:
-        print("No videos available for any of the channels.")
+        print("No live streams found for any channels.")
 
 if __name__ == "__main__":
-    # Use the provided API key based on the current hour
-    if now.hour >= 0 and now.hour < 3:
+    # Select API key based on current hour
+    if 0 <= now.hour < 3:
         api_key = "AIzaSyBX_LlRNOxBzT5eAWzRiCWNjFS000uqsBQ"  # allmybooks
-    elif now.hour >= 3 and now.hour < 6:
+    elif 3 <= now.hour < 6:
         api_key = "AIzaSyCgJaZsz-tsyAaIJRLc5NRYQyC-vnTCwAI"  # rokonmagura
-    elif now.hour >= 6 and now.hour < 9:
-        api_key = "AIzaSyA9AXuZ-x5tkUmaT5VmfWH4KFNc57NTEhA"  # amirokon1991
-    elif now.hour >= 9 and now.hour < 12:
-        api_key = "AIzaSyAd-mn7joueTcJoQWL3nBW2sHzsoDJtAfM"  # deshirambo5
-    elif now.hour >= 12 and now.hour < 15:
-        api_key = "AIzaSyAXq5VQlni9K7AKk1w2iU-EOCenzL8l4rA"  # 4kshort2021
-    elif now.hour >= 15 and now.hour < 18:
-        api_key = "AIzaSyAZuIBq2gMLU9josSi3zTW9Fnp_djzmlLM"  # deshirambo
-    elif now.hour >= 18 and now.hour < 21:
-        api_key = "AIzaSyBBYUHVcfHZ56lgsZdNB5W_WAUEgyh2hfQ"  # onlinesoft427
-    elif now.hour >= 21 and now.hour < 24:
-        api_key = "AIzaSyC-AStAvJQP0579qUvljAE4mV2X9eRBroo"  # deshirambo10
+    elif 6 <= now.hour < 9:
+        api_key = "AIzaSyA7ySJB3-FKUUWFpcT6PpCcF578936a8jQ"  # deshirambo11 (removed comma)
+    elif 9 <= now.hour < 12:
+        api_key = "AIzaSyA_58ry30FZ2qUQFNiJXJgNXJfCYj-0-9k"  # deshirambo12
+    elif 12 <= now.hour < 15:
+        api_key = "AIzaSyDYyv5PWLhOCRxAwMkiaH9VY_BXwMi3VzM"  # deshirambo13
+    elif 15 <= now.hour < 18:
+        api_key = "AIzaSyDJ5CkvzxGaJL99SdGqENypUVcm0nFaKEQ"  # deshirambo14
+    elif 18 <= now.hour < 21:
+        api_key = "AIzaSyDO8JaYU6HbD8PdypJhG-EkFi4nojq0hrE"  # deshirambo15
+    else:  # 21 <= now.hour < 24
+        api_key = "AIzaSyCxklIr0fXmsjmiwzoDfBBT0DxtMpWQS68"  # deshirambo16
 
     main(api_key)
