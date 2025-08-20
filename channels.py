446241316b9afd8e9046efd6636bda1989d7e0aa
Muhammad Rokonuzzaman <rokonmagura@gmail.com>
2025-08-20 17:54:05 +0600

    Update channels.py

--- channels.py
+++ channels.py
@@ -2,85 +2,71 @@
 channel_metadata = {
     # --- News Channels (Bangladesh) ---
     'UCWVqdPTigfQ-cSNwG7O9MeA': {  # EKHON TV
-        'channel_number': 103,
         'group_title': 'News',
         'channel_name': 'EKHON TV',
         'channel_logo': 'https://yt3.googleusercontent.com/66cO7vPXs2Xssf6fq2cn90oDsJ3OFMThb57qfRkRMjaSqg3ouTG6m9WQKZFg6GmUS5G8wkPu7ik=s72-c-k-c0x00ffffff-no-rj',
     },
     'UCHLqIOMPk20w-6cFgkA90jw': {  # Channel 24
-        'channel_number': 104,
         'group_title': 'News',
         'channel_name': 'Channel 24',
         'channel_logo': 'https://yt3.googleusercontent.com/8Q8MCd6ypr2Hzbp60VE_stJPl063kQYfeTxdIQkAXRfhdzxByLl0sJYHsk43uTM4W_cOzwcbPQ=s160-c-k-c0x00ffffff-no-rj',
     },
     'UCtqvtAVmad5zywaziN6CbfA': {  # Ekattor TV
-        'channel_number': 105,
         'group_title': 'News',
         'channel_name': 'Ekattor TV',
         'channel_logo': 'https://yt3.googleusercontent.com/M8Rqad6_uN86mMSvd9KGkE5G2mrVAgvfTV-VCsQb6jhfF5hEbcQCEJiInih4wb2fMQ_RG7Ku=s160-c-k-c0x00ffffff-no-rj',
     },
     'UCATUkaOHwO9EP_W87zCiPbA': {  # Independent Television
-        'channel_number': 106,
         'group_title': 'News',
         'channel_name': 'Independent Television',
         'channel_logo': 'https://yt3.googleusercontent.com/JO8MicN497ze8WVXcu-wmA2WAMqhO8UIQhslV3VhiRu1kaQU3r9nOB4IVkmUt0ALC23DVSSp=s160-c-k-c0x00ffffff-no-rj',
     },
     'UCUvXoiDEKI8VZJrr58g4VAw': {  # DBC NEWS
-        'channel_number': 107,
         'group_title': 'News',
         'channel_name': 'DBC News',
         'channel_logo': 'https://yt3.googleusercontent.com/ytc/AIdro_mcjICfjqYwOn4eljNmtsZLBpOmW0t0JlzJUhXSslAkBVo=s160-c-k-c0x00ffffff-no-rj',
     },
     'UC2P5Fd5g41Gtdqf0Uzh8Qaw': {  # Rtv News
-        'channel_number': 108,
         'group_title': 'News',
         'channel_name': 'Rtv News',
         'channel_logo': 'https://yt3.googleusercontent.com/Jw5N_GsIqsDis5cvYkAlJZFU2Z3m_6q6GaTdmUK0hJ4bsadl4He51sFu1LoCGKmf3QiTG-9P5G4=s160-c-k-c0x00ffffff-no-rj',
     },
     'UC0V3IJCnr6ZNjB9t_GLhFFA': {  # NTV Live
-        'channel_number': 109,
         'group_title': 'News',
         'channel_name': 'NTV Live',
         'channel_logo': 'https://yt3.googleusercontent.com/NgVaE3RsOhC9cIV_6kTp0h2ikrHIFG8QPJF5IrRg_nPiQbQeG-dzK5SHLmi_MDEyVNj73aeSHg=s160-c-k-c0x00ffffff-no-rj',
     },
     'UC8NcXMG3A3f2aFQyGTpSNww': {  # Channel i News
-        'channel_number': 110,
         'group_title': 'News',
         'channel_name': 'Channel i News',
         'channel_logo': 'https://yt3.googleusercontent.com/Yv5b1h00Dj_JkJsCD8IEuodFSRq3hFiFi0AQc-W5JrJkrAyX98lxWGUUhjqZzOA-NU5LwUzU=s160-c-k-c0x00ffffff-no-rj',
     },
     'UCb2O5Uo4a26CdTE7_2QA-jA': {  # NEWS24 Television
-        'channel_number': 111,
         'group_title': 'News',
         'channel_name': 'NEWS24 Television',
         'channel_logo': 'https://yt3.googleusercontent.com/bn7BHmnbqkIJMoQWzUk3K5Wzzt_mVgVdjQ5XV8PWdQS18_w4ZVZOSFwe_ZIKaO3KitQPVuQczA=s160-c-k-c0x00ffffff-no-rj',
     },
     'UCRt2klyaxgx89vPF8cFMfnQ': {  # Kalbela News
-        'channel_number': 113,
         'group_title': 'News',
         'channel_name': 'Kalbela News',
         'channel_logo': 'https://yt3.googleusercontent.com/DdtUl06VkqvZvr9aDFP6iX-qxWxV5Aqlk1d4mTUdD1E34wwX333DKo56iJiSJ3hojEeeW_kVzEc=s160-c-k-c0x00ffffff-no-rj',
     },
     'UC9Rgo0CrNyd7OWliLekqqGA': {  # ATN News
-        'channel_number': 114,
         'group_title': 'News',
         'channel_name': 'ATN News',
         'channel_logo': 'https://yt3.googleusercontent.com/ceDID2koAfvhvykNroiBjW4SkTrkGVrjFk1EkWRz8SZdhV_dFsCtuJ3w9l0D-y06VgHjvB-WqA=s160-c-k-c0x00ffffff-no-rj',
     },
     'UCxHoBXkY88Tb8z1Ssj6CWsQ': {  # SOMOY NEWS
-        'channel_number': 115,
         'group_title': 'News',
         'channel_name': 'SOMOY NEWS',
         'channel_logo': 'https://yt3.googleusercontent.com/e5-tNNraLRpGlwIoDtdKvaqVzqxswudmLbQXyTMPqYVdxLWTmz1TJRPC4G0PbOYVJta_oGZAUGs=s72-c-k-c0x00ffffff-no-rj',
     },
     'UCPREnbhKQP-hsVfsfKP-mCw': {  # NEWS24
-        'channel_number': 116,
         'group_title': 'News',
         'channel_name': 'NEWS24',
         'channel_logo': 'https://yt3.googleusercontent.com/FK8kaDWHXG4F4yCVGbGP9gE5hNOOTBTXof6KFMrhj3BZ2yW0oHy7PxJRRP8QdMZfGWCNTG3L4g=s72-c-k-c0x00ffffff-no-rj',
     },
     'UCUDQdVsKssximyFwg4IxnOQ': {  # NTV News
-        'channel_number': 117,
         'group_title': 'News',
         'channel_name': 'NTV News',
         'channel_logo': 'https://yt3.googleusercontent.com/ZcYmoynvuv7AIJwnPon5Hq10hNUK2Ct-TA-nYvW8joYyvbAG9ZCg9aiUypHdpfzY-xT7R5KnJqU=s72-c-k-c0x00ffffff-no-rj',
@@ -88,57 +74,48 @@ channel_metadata = {
 
     # --- News Channels (India) ---
     'UCbf0XHULBkTfv2hBjaaDw9Q': {  # News18 Bangla
-        'channel_number': 121,
-        'group_title': 'News',
+        'group_title': 'News India',
         'channel_name': 'News18 Bangla',
         'channel_logo': 'https://yt3.googleusercontent.com/0pVAsTdgTX-iREI9xZUMsYbqjpclujOiC7mocZIvLlWYVmhKRP131kzwVzM-i63lQz2YMMXo=s160-c-k-c0x00ffffff-no-rj',
     },
     'UCdF5Q5QVbYstYrTfpgUl0ZA': {  # Zee 24 Ghanta
-        'channel_number': 122,
-        'group_title': 'News',
+        'group_title': 'News India',
         'channel_name': 'Zee 24 Ghanta',
         'channel_logo': 'https://yt3.googleusercontent.com/ou3JhDnH8ChdzRKooH5hGjTRGKpr9dGhi7lv7QW2zgmrnme0HbPKc8qI3yu8ZdI6NZna-CdJFw=s160-c-k-c0x00ffffff-no-rj',
     },
     'UCHCR4UFsGwd_VcDa0-a4haw': {  # TV9 Bangla
-        'channel_number': 123,
-        'group_title': 'News',
+        'group_title': 'News India',
         'channel_name': 'TV9 Bangla',
         'channel_logo': 'https://yt3.googleusercontent.com/d8QNkJ7Jby9hVSTm67-E4nfbI-7CTgP262NPVGfYpoTZaxLw7uAOPxs5dJtARjEFQijsRsuiFQ=s160-c-k-c0x00ffffff-no-rj',
     },
     'UCajVjEHDoVn_AHsunUZz_EQ': {  # Republic Bangla
-        'channel_number': 124,
-        'group_title': 'News',
+        'group_title': 'News India',
         'channel_name': 'Republic Bangla',
         'channel_logo': 'https://yt3.googleusercontent.com/ytc/AIdro_nvgVv3ZKeS6nLI_ZGZw-pZpt88bQpFPZNbUOADI3Bvz9k=s160-c-k-c0x00ffffff-no-rj',
     },
     'UCv3rFzn-GHGtqzXiaq3sWNg': {  # ABP ANANDA
-        'channel_number': 125,
-        'group_title': 'News',
+        'group_title': 'News India',
         'channel_name': 'ABP Ananda',
         'channel_logo': 'https://yt3.googleusercontent.com/ytc/AIdro_kLTXHZwzmSJJh3W6bm_134dfLEh_vjEpjL8QE8Yn4l6cs=s160-c-k-c0x00ffffff-no-rj',
     },
 
     # --- International News Channels ---
     'UCNye-wNBqNL5ZzHSJj3l8Bg': {  # Al Jazeera English
-        'channel_number': 150,
         'group_title': 'International News',
         'channel_name': 'Al Jazeera English',
         'channel_logo': '',
     },
     'UC7fWeaHhqgM4Ry-RMpM2YYw': {  # TRT World
-        'channel_number': 151,
         'group_title': 'International News',
         'channel_name': 'TRT World',
         'channel_logo': '',
     },
     'UCknLrEdhRCp1aegoMqRaCZg': {  # DW News
-        'channel_number': 152,
         'group_title': 'International News',
         'channel_name': 'DW News',
         'channel_logo': '',
     },
     'UCUMZ7gohGI9HcU9VNsr2FJQ': {  # Bloomberg Originals
-        'channel_number': 153,
         'group_title': 'International News',
         'channel_name': 'Bloomberg Originals',
         'channel_logo': '',
@@ -146,77 +123,47 @@ channel_metadata = {
 
     # --- Entertainment Channels ---
     'UC9nuJbEL-AMJLLqc2-ej8xQ': {  # Bongo
-        'channel_number': 201,
         'group_title': 'Entertainment',
         'channel_name': 'Bongo',
         'channel_logo': '',
     },
     'UCvoC1eVphUAe7a0m-uuoPbg': {  # Bongo Movies
-        'channel_number': 202,
         'group_title': 'Entertainment',
         'channel_name': 'Bongo Movies',
         'channel_logo': '',
     },
     'UCsr6QVeLlkitleHoS0T4IxQ': {  # Banglavision DRAMA
-        'channel_number': 203,
         'group_title': 'Entertainment',
         'channel_name': 'Banglavision DRAMA',
-        'channel_logo': '',
-    },
-    'UC5V8MdQsT_gLlw8rTyf7jVQ': {  # ClipZone: Comedy Callbacks
-        'channel_number': 206,
-        'group_title': 'Entertainment',
-        'channel_name': 'ClipZone: Comedy Callbacks',
-        'channel_logo': '',
-    },
-    'UCQt7Z-GE0wF8AzFNvQEqjvg': {  # ClipZone: Heroes & Villains
-        'channel_number': 207,
-        'group_title': 'Entertainment',
-        'channel_name': 'ClipZone: Heroes & Villains',
-        'channel_logo': '',
-    },
-    'UCw7SNYrYei7F5ttQO3o-rpA': {  # Disney Channel
-        'channel_number': 208,
-        'group_title': 'Entertainment',
-        'channel_name': 'Disney Channel',
         'channel_logo': '',
     },
 
     # --- Religious Channels ---
      'UCzYIB8kZ_3dpeYKEJ6OrP7w': {  # NTV Islamic Show
-        'channel_number': 300,
         'group_title': 'Religious',
         'channel_name': 'NTV Islamic Show',
         'channel_logo': 'https://yt3.googleusercontent.com/yQS-hwvcZQRD8Kr-kqiCCT0FLbFVW7ZLfmjpT8caI2LX4CdoIlZOLSj04JqOoHnvfTSmibJDRJg=s72-c-k-c0x00ffffff-no-rj',
     },
 
-    # --- Wildlife and Educational Channels ---
-
     # --- Kids Channels ---
     'UCu7IDy0y-ZA0qaG51wrQY6w': {  # Curious George Official
-        'channel_number': 403,
         'group_title': 'Kids',
         'channel_name': 'Curious George Official',
         'channel_logo': '',
     },
     'UCVzLLZkDuFGAE2BGdBuBNBg': {  # Bluey - Official Channel
-        'channel_number': 404,
         'group_title': 'Kids',
         'channel_name': 'Bluey - Official Channel',
         'channel_logo': '',
     },
     'UCoBpC9J2EcbAMprw7YjC93A': {  # The Amazing World of Gumball
-        'channel_number': 405,
         'group_title': 'Kids',
         'channel_name': 'The Amazing World of Gumball',
         'channel_logo': '',
     },
     'UCu59yAFE8fM0sVNTipR4edw': {  # Masha and The Bear
-        'channel_number': 409,
         'group_title': 'Kids',
         'channel_name': 'Masha and The Bear',
         'channel_logo': '',
     },
-
 }
-
