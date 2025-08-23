5f9120df759de7fea44277878c0447be18bcc117
Muhammad Rokonuzzaman <rokonmagura@gmail.com>
2025-08-24 01:44:18 +0600

    Create check_API.py

---
+++ obsolete/check_API.py
@@ -0,0 +1,17 @@
+from googleapiclient.discovery import build
+
+keys = [
+    "AIzaSyCgJaZsz-tsyAaIJRLc5NRYQyC-vnTCwAI",  # rokonmagura
+    "AIzaSyBX_LlRNOxBzT5eAWzRiCWNjFS000uqsBQ",  # allmybooks
+    "AIzaSyBQYlMpMxV9tmKl9GmrxldwZhOrf7NCfs4",  # farzanazamanmunia
+    "AIzaSyCzk9DwsciObuhF3sNUbX1BdBBt0sNRwOw", # deshirambo19
+]
+
+for key in keys:
+    try:
+        youtube = build("youtube", "v3", developerKey=key)
+        req = youtube.channels().list(part="snippet", id="UC_x5XG1OV2P6uZZ5FSM9Ttw") # Google Developers
+        res = req.execute()
+        print(f"✅ Working: {key}")
+    except Exception as e:
+        print(f"❌ Bad: {key} - {e}")
