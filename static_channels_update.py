595c7babd1d0610ee5bb8abf70e1740b89a6f9f8
Muhammad Rokonuzzaman <rokonmagura@gmail.com>
2025-09-06 01:24:17 +0600

    Update static_channels_update.py

--- static_channels_update.py
+++ static_channels_update.py
@@ -12,7 +12,7 @@ print = functools.partial(print, flush=True)
 JSON_FILE = "static_channels.json"
 FAST_MODE = False  # True = fast FFmpeg, False = full/slow check
 RETRIES = 3
-MAX_WORKERS = 60  # Parallel FFmpeg threads
+MAX_WORKERS = 80  # Parallel FFmpeg threads
 EXCLUDE_LIST = ["Republic Bangla",
                 "Republic Bharat",
                 "Aaj Tak HD",
