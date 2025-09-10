6ed7cde19d0023db3fefcc15bab3a6a63a9559d0
Muhammad Rokonuzzaman <rokonmagura@gmail.com>
2025-09-10 23:06:51 +0600

    Update YT_channels.py

--- YT_channels.py
+++ YT_channels.py
@@ -3,84 +3,84 @@ channel_metadata = {
     # --- News Channels (Bangladesh) ---
     'UCWVqdPTigfQ-cSNwG7O9MeA': {  # EKHON TV
         'group_title': 'Bangla News',
-        'channel_name': 'EKHON TV',
+        'channel_name': 'BD | EKHON TV',
         'channel_logo': 'https://yt3.googleusercontent.com/66cO7vPXs2Xssf6fq2cn90oDsJ3OFMThb57qfRkRMjaSqg3ouTG6m9WQKZFg6GmUS5G8wkPu7ik=s72-c-k-c0x00ffffff-no-rj',
     },
     'UCHLqIOMPk20w-6cFgkA90jw': {  # Channel 24
         'group_title': 'Bangla News',
-        'channel_name': 'Channel 24',
+        'channel_name': 'BD | Channel 24',
         'channel_logo': 'https://yt3.googleusercontent.com/8Q8MCd6ypr2Hzbp60VE_stJPl063kQYfeTxdIQkAXRfhdzxByLl0sJYHsk43uTM4W_cOzwcbPQ=s160-c-k-c0x00ffffff-no-rj',
     },
     'UCtqvtAVmad5zywaziN6CbfA': {  # Ekattor TV
         'group_title': 'Bangla News',
-        'channel_name': 'Ekattor TV',
+        'channel_name': 'BD | Ekattor TV',
         'channel_logo': 'https://yt3.googleusercontent.com/M8Rqad6_uN86mMSvd9KGkE5G2mrVAgvfTV-VCsQb6jhfF5hEbcQCEJiInih4wb2fMQ_RG7Ku=s160-c-k-c0x00ffffff-no-rj',
     },
     'UCATUkaOHwO9EP_W87zCiPbA': {  # Independent Television
         'group_title': 'Bangla News',
-        'channel_name': 'Independent Television',
+        'channel_name': 'BD | Independent Television',
         'channel_logo': 'https://yt3.googleusercontent.com/JO8MicN497ze8WVXcu-wmA2WAMqhO8UIQhslV3VhiRu1kaQU3r9nOB4IVkmUt0ALC23DVSSp=s160-c-k-c0x00ffffff-no-rj',
     },
     'UCUvXoiDEKI8VZJrr58g4VAw': {  # DBC NEWS
         'group_title': 'Bangla News',
-        'channel_name': 'DBC News',
+        'channel_name': 'BD | DBC News',
         'channel_logo': 'https://yt3.googleusercontent.com/ytc/AIdro_mcjICfjqYwOn4eljNmtsZLBpOmW0t0JlzJUhXSslAkBVo=s160-c-k-c0x00ffffff-no-rj',
     },
     'UC2P5Fd5g41Gtdqf0Uzh8Qaw': {  # Rtv News
         'group_title': 'Bangla News',
-        'channel_name': 'Rtv News',
+        'channel_name': 'BD | Rtv News',
         'channel_logo': 'https://yt3.googleusercontent.com/Jw5N_GsIqsDis5cvYkAlJZFU2Z3m_6q6GaTdmUK0hJ4bsadl4He51sFu1LoCGKmf3QiTG-9P5G4=s160-c-k-c0x00ffffff-no-rj',
     },
     'UC0V3IJCnr6ZNjB9t_GLhFFA': {  # NTV Live
         'group_title': 'Bangla News',
-        'channel_name': 'NTV Live',
+        'channel_name': 'BD | NTV Live',
         'channel_logo': 'https://yt3.googleusercontent.com/NgVaE3RsOhC9cIV_6kTp0h2ikrHIFG8QPJF5IrRg_nPiQbQeG-dzK5SHLmi_MDEyVNj73aeSHg=s160-c-k-c0x00ffffff-no-rj',
     },
     'UC8NcXMG3A3f2aFQyGTpSNww': {  # Channel i News
         'group_title': 'Bangla News',
-        'channel_name': 'Channel i News',
+        'channel_name': 'BD | Channel i News',
         'channel_logo': 'https://yt3.googleusercontent.com/Yv5b1h00Dj_JkJsCD8IEuodFSRq3hFiFi0AQc-W5JrJkrAyX98lxWGUUhjqZzOA-NU5LwUzU=s160-c-k-c0x00ffffff-no-rj',
     },
     'UCb2O5Uo4a26CdTE7_2QA-jA': {  # NEWS24 Television
         'group_title': 'Bangla News',
-        'channel_name': 'NEWS24 Television',
+        'channel_name': 'BD | NEWS24 Television',
         'channel_logo': 'https://yt3.googleusercontent.com/bn7BHmnbqkIJMoQWzUk3K5Wzzt_mVgVdjQ5XV8PWdQS18_w4ZVZOSFwe_ZIKaO3KitQPVuQczA=s160-c-k-c0x00ffffff-no-rj',
     },
     'UCRt2klyaxgx89vPF8cFMfnQ': {  # Kalbela News
         'group_title': 'Bangla News',
-        'channel_name': 'Kalbela News',
+        'channel_name': 'BD | Kalbela News',
         'channel_logo': 'https://yt3.googleusercontent.com/DdtUl06VkqvZvr9aDFP6iX-qxWxV5Aqlk1d4mTUdD1E34wwX333DKo56iJiSJ3hojEeeW_kVzEc=s160-c-k-c0x00ffffff-no-rj',
     },
     'UC9Rgo0CrNyd7OWliLekqqGA': {  # ATN News
         'group_title': 'Bangla News',
-        'channel_name': 'ATN News',
+        'channel_name': 'BD | ATN News',
         'channel_logo': 'https://yt3.googleusercontent.com/ceDID2koAfvhvykNroiBjW4SkTrkGVrjFk1EkWRz8SZdhV_dFsCtuJ3w9l0D-y06VgHjvB-WqA=s160-c-k-c0x00ffffff-no-rj',
     },
     'UCxHoBXkY88Tb8z1Ssj6CWsQ': {  # SOMOY NEWS
         'group_title': 'Bangla News',
-        'channel_name': 'SOMOY NEWS',
+        'channel_name': 'BD | SOMOY NEWS',
         'channel_logo': 'https://yt3.googleusercontent.com/e5-tNNraLRpGlwIoDtdKvaqVzqxswudmLbQXyTMPqYVdxLWTmz1TJRPC4G0PbOYVJta_oGZAUGs=s72-c-k-c0x00ffffff-no-rj',
     },
     'UCPREnbhKQP-hsVfsfKP-mCw': {  # NEWS24
         'group_title': 'Bangla News',
-        'channel_name': 'NEWS24',
+        'channel_name': 'BD | NEWS24',
         'channel_logo': 'https://yt3.googleusercontent.com/FK8kaDWHXG4F4yCVGbGP9gE5hNOOTBTXof6KFMrhj3BZ2yW0oHy7PxJRRP8QdMZfGWCNTG3L4g=s72-c-k-c0x00ffffff-no-rj',
     },
     'UCUDQdVsKssximyFwg4IxnOQ': {  # NTV News
         'group_title': 'Bangla News',
-        'channel_name': 'NTV News',
+        'channel_name': 'BD | NTV News',
         'channel_logo': 'https://yt3.googleusercontent.com/ZcYmoynvuv7AIJwnPon5Hq10hNUK2Ct-TA-nYvW8joYyvbAG9ZCg9aiUypHdpfzY-xT7R5KnJqU=s72-c-k-c0x00ffffff-no-rj',
     },
 
     # --- News Channels (India) ---
     'UCbf0XHULBkTfv2hBjaaDw9Q': {  # News18 Bangla
         'group_title': 'Bangla News',
-        'channel_name': 'News18 Bangla',
+        'channel_name': 'IN | News18 Bangla',
         'channel_logo': 'https://yt3.googleusercontent.com/0pVAsTdgTX-iREI9xZUMsYbqjpclujOiC7mocZIvLlWYVmhKRP131kzwVzM-i63lQz2YMMXo=s160-c-k-c0x00ffffff-no-rj',
     },
     'UCdF5Q5QVbYstYrTfpgUl0ZA': {  # Zee 24 Ghanta
         'group_title': 'Bangla News',
-        'channel_name': 'Zee 24 Ghanta',
+        'channel_name': 'IN | Zee 24 Ghanta',
         'channel_logo': 'https://yt3.googleusercontent.com/ou3JhDnH8ChdzRKooH5hGjTRGKpr9dGhi7lv7QW2zgmrnme0HbPKc8qI3yu8ZdI6NZna-CdJFw=s160-c-k-c0x00ffffff-no-rj',
     },
     # 'UCHCR4UFsGwd_VcDa0-a4haw': {  # TV9 Bangla
@@ -127,6 +127,7 @@ channel_metadata = {
         'channel_logo': 'https://yt3.googleusercontent.com/plbYMBlbSJHXOLEqCE4tCFPkw-WwoZ5y-oI4M1l5dljjOFMrQj9g_ZYTb5Fu8t_y8weRNR4iHQ=s160-c-k-c0x00ffffff-no-rj',
     },
 }
+
 
 
 
