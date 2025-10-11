348a2362ec31341396a6f9a3578f66778769fd89
Muhammad Rokonuzzaman <rokonmagura@gmail.com>
2025-10-11 16:18:13 +0600

    fixed error

--- scripts/YT_get_YouTube_link.py
+++ scripts/YT_get_YouTube_link.py
@@ -4,7 +4,7 @@ import datetime
 import yt_dlp
 import os
 import logging
-from code.YT_channels import channel_metadata
+from scripts.YT_channels import channel_metadata
 
 # --- Setup logging ---
 logger = logging.getLogger("yt_logger")
